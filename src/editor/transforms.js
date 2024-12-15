bff98835afe7e6d70b8e0af41a611be5b679e658
Cory Hughart <cory@coryhughart.com>
2024-12-15 13:42:14 -0600

    return empty string if transform is invalid

--- src/editor/transforms.js
+++ src/editor/transforms.js
@@ -4,37 +4,50 @@
 
 import { addFilter } from './hooks';
 
+addFilter( 'termQuery.termMetaTransform.attachment_id_to_url', 'term-query/term-meta-transform/attachment_id_to_url', attachment_id_to_url );
+addFilter( 'termQuery.termMetaTransform.attachment_id_to_image_alt', 'term-query/term-meta-transform/attachment_id_to_image_alt', attachment_id_to_image_alt );
+
 /**
  * Transform to attachment URL.
+ *
+ * @param {number} value Attachment ID.
+ * @param {Object} args Arguments.
+ * @param {Function} select Select function.
+ * @return {string} Attachment URL or empty string.
  */
-addFilter( 'termQuery.termMetaTransform.attachment_id_to_url', 'term-query/term-meta-transform/attachment_id_to_url', ( value, args, select ) => {
+const attachment_id_to_url = ( value, args, select ) => {
 	// If value is not numeric, return early.
 	if ( isNaN( value ) ) {
-		return null;
+		return '';
 	}
 
 	const attachment = select( 'core' ).getMedia( value );
 	if ( ! attachment ) {
-		return null;
+		return '';
 	}
 
 	const size = args?.size ?? 'full';
 	return attachment.media_details.sizes[ size ]?.source_url ?? attachment.source_url;
-} );
+};
 
 /**
  * Transform to attachment image alt.
+ *
+ * @param {number} value Attachment ID.
+ * @param {Object} args Arguments.
+ * @param {Function} select Select function.
+ * @return {string} Attachment image alt or empty string.
  */
-addFilter( 'termQuery.termMetaTransform.attachment_id_to_image_alt', 'term-query/term-meta-transform/attachment_id_to_image_alt', ( value, args, select ) => {
+const attachment_id_to_image_alt = ( value, args, select ) => {
 	// If value is not numeric, return early.
 	if ( isNaN( value ) ) {
-		return null;
+		return '';
 	}
 
 	const attachment = select( 'core' ).getMedia( value );
 	if ( ! attachment ) {
-		return null;
+		return '';
 	}
 
 	return attachment.alt_text;
-} );
+};
