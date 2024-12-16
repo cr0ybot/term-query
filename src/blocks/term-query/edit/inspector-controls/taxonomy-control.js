9c3e4c73900b8dbaa59593a37433b8dc249f26eb
Cory Hughart <cory@coryhughart.com>
2024-12-16 10:17:09 -0600

    read-only taxonomy if inherited

--- src/blocks/term-query/edit/inspector-controls/taxonomy-control.js
+++ src/blocks/term-query/edit/inspector-controls/taxonomy-control.js
@@ -1,27 +1,42 @@
-import { ComboboxControl } from '@wordpress/components';
+import {
+	ComboboxControl,
+	TextControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 import { useTaxonomies } from '../../utils';
 
-export default function TaxonomyControl( { onChange, taxonomy } ) {
+export default function TaxonomyControl( { onChange, taxonomy, inherited } ) {
 	const taxonomies = useTaxonomies();
 	if ( ! taxonomies || taxonomies.length === 0 ) {
 		return null;
 	}
 
-	return (
-		<>
-			<ComboboxControl
+	if ( inherited ) {
+		const label = taxonomies.find( ( { slug } ) => slug === taxonomy )?.name ?? taxonomy;
+
+		return (
+			<TextControl
 				__nextHasNoMarginBottom
 				__next40pxDefaultSize
 				label={ __( 'Taxonomy', 'term-query' ) }
-				value={ taxonomy }
-				onChange={ onChange }
-				options={ taxonomies.map( ( { slug, name } ) => ( {
-					value: slug,
-					label: name,
-				} ) ) }
+				value={ label }
+				disabled
 			/>
-		</>
+		);
+	}
+
+	return (
+		<ComboboxControl
+			__nextHasNoMarginBottom
+			__next40pxDefaultSize
+			label={ __( 'Taxonomy', 'term-query' ) }
+			value={ taxonomy }
+			onChange={ onChange }
+			options={ taxonomies.map( ( { slug, name } ) => ( {
+				value: slug,
+				label: name,
+			} ) ) }
+		/>
 	);
 }
